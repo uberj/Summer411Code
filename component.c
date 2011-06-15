d18a69d6b0b7720529563046ab1f8e0f013bcb1b
uberj <uberj@youremail.com>
2011-06-15 12:16:50 -0700

    it works!

--- component.c
+++ component.c
@@ -2,40 +2,41 @@
 #include <string.h>
 #include <stdlib.h>
 #define GRAPH_FILE "graph.txt"
-#define NUM 3
+#define NUM 26
 
 struct node {
-    struct node* neighbors;
+    struct node** neighbors; //immutable
+    struct node** _view;
     char letter;
-    int mark;
+    unsigned int *marks;
     int n_lock;
 };
 
-void *init_nodes(struct node** nodes, int count)
+void *init_nodes(struct node* nodes, int count)
 {
     int i;
     for(i=0;i<NUM;i++) {
-        nodes[i] = calloc(1,sizeof(struct node));
-        nodes[i]->letter = 0;
-        nodes[i]->letter = 0;
-        nodes[i]->neighbors = calloc(NUM, sizeof(struct node));
+        nodes[i].neighbors = calloc(NUM, sizeof(struct node*));
+        nodes[i]._view = calloc(NUM, sizeof(struct node*));
+        nodes[i].marks = calloc(NUM, sizeof(unsigned int));
     }
     return;
 
 }
 
-void *_print_nodes(struct node** nodes, int count)
+void *_print_nodes(struct node* nodes, int count)
 {
     int i,j;
     for(i=0;i<NUM;i++) {
         printf("%c: ",i+65);
         for(j=0;j<NUM;j++){
-            if(! nodes[i]->neighbors[j].letter ) {
+            if(!nodes[i]._view[j]) {
                 // None is letter
                 printf(" ,");
                 continue;
             }else{
-                printf("%c,",nodes[i]->neighbors[j].letter);
+                //printf("%c,",nodes[i].neighbors[j]->letter);
+                printf("%c,",nodes[i]._view[j]->letter);
             }
         }
         printf("\n");
@@ -44,24 +45,35 @@ void *_print_nodes(struct node** nodes, int count)
 
 }
 
-void *_generate_ds(FILE* fp,struct node** nodes,size_t *count)
+void *_generate_ds(FILE* fp,struct node* nodes)
 {
     char unit[7];
-    int i,a_node,a_edge;
+    int i,count,a_node,a_edge;
+    count = 0;;
     while(fgets(unit,8,fp)) {
-        *count++;
+        count++;
         a_node = (int)(unit[0]-65);
         a_edge = (int)(unit[3]-65);
         printf("%i %c,%i %c\n",a_node,unit[0],a_edge,unit[3]);
         // Deal with node
-        nodes[a_node]->letter = unit[0]; // Redundant
-        nodes[a_node]->neighbors[a_node] = *nodes[a_node];
-        nodes[a_node]->neighbors[a_edge] = *nodes[a_edge];
-        // Give letter to edge
-        nodes[a_edge]->letter = unit[3];
+        nodes[a_node].letter = unit[0]; // Redundant
+        nodes[a_edge].letter = unit[3]; // Redundant
 
+        // This will need to change if you want to have an directed graph.
+        // X connected to X
+        nodes[a_node].neighbors[a_node] = &nodes[a_node]; // Redundant
+        nodes[a_node]._view[a_node] = &nodes[a_node]; // Redundant
+        // Y connected to X
+        nodes[a_edge].neighbors[a_node] = &nodes[a_node];
+        nodes[a_edge]._view[a_node] = &nodes[a_node];
+        // X connected to Y
+        nodes[a_node].neighbors[a_edge] = &nodes[a_edge];
+        nodes[a_node]._view[a_edge] = &nodes[a_edge];
+        // Y connected to Y
+        nodes[a_edge].neighbors[a_edge] = &nodes[a_edge]; // Redundant
+        nodes[a_edge]._view[a_edge] = &nodes[a_edge]; // Redundant
     }
-
+    // Now make a mutable _view array for each node.
     return;
 }
 
@@ -69,15 +81,19 @@ void *walk_node(struct node* static_node, struct node* local_node) {
     int i;
     struct node node_loc,node_stat;
     for(i=0;i<NUM;i++) {
-        if( ( local_node->neighbors[i].letter != 0 ) &&
-            ( local_node->neighbors[i].letter != static_node->letter)) {
-            // Local is next to i, Static doesn't know about i
-            static_node->neighbors[i].mark = 1;
-            static_node->neighbors[i] = local_node->neighbors[i];
-            walk_node( static_node, &local_node->neighbors[i] );
-
+        if( local_node->neighbors[i] == static_node ){
+            // We are looking at ourselves.
+            continue;
+        } else if ( local_node->neighbors[i] != 0 ){
+            if( static_node->_view[i] != local_node->neighbors[i] ){
+                static_node->_view[i] = local_node->neighbors[i];
+            }
+            if( static_node->marks[i] == 0 ){
+                // Need a way to do i+1 if we've been there before.
+                static_node->marks[i] = 1;
+                walk_node(static_node, static_node->_view[i]);
+            }
         }
-        printf("HERE\n");
     }
 
 }
@@ -87,7 +103,7 @@ int main(void)
     FILE *fp;
     size_t count = 0;
     int i;
-    struct node** nodes = calloc(NUM,sizeof(struct node*)); //A-Z
+    struct node* nodes = calloc(NUM,sizeof(struct node)); //A-Z
     init_nodes(nodes,count);
 
     fp = fopen(GRAPH_FILE, "r");
@@ -95,11 +111,11 @@ int main(void)
         perror("failed to open GRAPH_FILE");
         return EXIT_FAILURE;
     }
-    _generate_ds(fp,nodes,&count);
+    _generate_ds(fp,nodes);
     _print_nodes(nodes,count);
     // Do threads here
     for(i=0;i<NUM;i++){
-        walk_node(nodes[i],nodes[i]);
+        walk_node(&nodes[i],&nodes[i]);
     }
     // end threads
     printf("Crunched\n");
