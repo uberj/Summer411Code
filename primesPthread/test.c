8bdf5508b9af086c9b582b6453e4c857babea5bc
Matthew Atwood <atwoodm@kec1130-26.engr.oregonstate.edu>
2011-06-16 11:27:04 -0700

    initial import primes pthread implementation

---
+++ primesPthread/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (int argc, char *argv[]){
+	int num, result;
+	num = 9;
+	result = sqrt(num);
+	printf("the sq root for %d is %d", num, result);
+	return 0;
+}
