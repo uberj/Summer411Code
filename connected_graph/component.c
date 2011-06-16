dbbbad22900147bbfdeb40ab7288ac3dab0530a2
uberj <uberj@youremail.com>
2011-06-15 17:01:44 -0700

    working on making the threading scalable.

--- connected_graph/component.c
+++ connected_graph/component.c
@@ -1,8 +1,15 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #define GRAPH_FILE "graph.txt"
-#define NUM 26
+#define NUM_CPU 2
+#define NUM_NODES 26
+
+struct pt_arg {
+    struct node* start_node;
+    int size;
+};
 
 struct node {
     struct node** neighbors; //immutable
@@ -15,10 +22,10 @@ struct node {
 void *init_nodes(struct node* nodes, int count)
 {
     int i;
-    for(i=0;i<NUM;i++) {
-        nodes[i].neighbors = calloc(NUM, sizeof(struct node*));
-        nodes[i]._view = calloc(NUM, sizeof(struct node*));
-        nodes[i].marks = calloc(NUM, sizeof(unsigned int));
+    for(i=0;i<NUM_NODES;i++) {
+        nodes[i].neighbors = calloc(NUM_NODES, sizeof(struct node*));
+        nodes[i]._view = calloc(NUM_NODES, sizeof(struct node*));
+        nodes[i].marks = calloc(NUM_NODES, sizeof(unsigned int));
     }
     return;
 
@@ -27,9 +34,9 @@ void *init_nodes(struct node* nodes, int count)
 void *_print_nodes(struct node* nodes, int count)
 {
     int i,j;
-    for(i=0;i<NUM;i++) {
+    for(i=0;i<NUM_NODES;i++) {
         printf("%c: ",i+65);
-        for(j=0;j<NUM;j++){
+        for(j=0;j<NUM_NODES;j++){
             if(!nodes[i]._view[j]) {
                 // None is letter
                 printf(" ,");
@@ -73,14 +80,13 @@ void *_generate_ds(FILE* fp,struct node* nodes)
         nodes[a_edge].neighbors[a_edge] = &nodes[a_edge]; // Redundant
         nodes[a_edge]._view[a_edge] = &nodes[a_edge]; // Redundant
     }
-    // Now make a mutable _view array for each node.
     return;
 }
 
 void *walk_node(struct node* static_node, struct node* local_node) {
     int i;
     struct node node_loc,node_stat;
-    for(i=0;i<NUM;i++) {
+    for(i=0;i<NUM_NODES;i++) {
         if( local_node->neighbors[i] == static_node ){
             // We are looking at ourselves.
             continue;
@@ -95,16 +101,52 @@ void *walk_node(struct node* static_node, struct node* local_node) {
             }
         }
     }
-
 }
+
+void *init_arg(struct node *nodes, struct pt_arg* pt_arg,int size){
+    int i,node_count,node_per_thread,extra;
+    node_per_thread = NUM_NODES/NUM_CPU;
+    extra = NUM_NODES%node_per_thread;
+    for(i=0;i<NUM_CPU;i++){
+        pt_arg[i].start_node = &nodes[i*node_per_thread];
+        pt_arg[i].size = node_per_thread;
+    }
+    pt_arg[i-1].size += extra;
+}
+void *calc_connected(void *ptg_arg) {
+    int i;
+    struct node* start_node;
+    for(i=0;i<ptg_arg;i++){
+        if(((struct node*)start_node)->letter == 0){
+            printf("Empty node\n");
+            pthread_exit(NULL);
+        }
+        printf("Non-Empty node\n");
+        walk_node( (struct node *)start_node, (struct node *)start_node );
+    }
+    printf("Done with node: %c\n",((struct node*)start_node)->letter);
+    pthread_exit(NULL);
+}
+
 
 int main(void)
 {
     FILE *fp;
     size_t count = 0;
-    int i;
-    struct node* nodes = calloc(NUM,sizeof(struct node)); //A-Z
+    int i,j,rc,numb_threads;
+    void * status;
+    struct node* nodes = calloc(NUM_NODES,sizeof(struct node)); //A-Z
+    pthread_t threads[NUM_CPU];
+    pthread_attr_t attr;
+    struct pt_arg pt_arg[NUM_CPU];
+
+    /* We want to joni these threads later. */
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+
     init_nodes(nodes,count);
+    init_arg(nodes,pt_arg,NUM_CPU);
 
     fp = fopen(GRAPH_FILE, "r");
     if(fp == NULL) {
@@ -114,13 +156,34 @@ int main(void)
     _generate_ds(fp,nodes);
     _print_nodes(nodes,count);
     // Do threads here
-    for(i=0;i<NUM;i++){
-        walk_node(&nodes[i],&nodes[i]);
+    /*
+        We need to divide up the workload to each CPU
+        node_per_thread = NUM_NODES/NUM_CPU
+    */
+    for(i=0;i<NUM_CPU;i++){
+        rc = pthread_create (&threads[i], &attr, calc_connected, (void *)pt_arg[i]);
+        if (rc){
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+        //calc_connected(&nodes[i],&nodes[i]);
     }
     // end threads
+    pthread_attr_destroy(&attr);
+    for(i=0; i<NUM_CPU; i++) {
+        rc = pthread_join(threads[i], &status);
+        if (rc) {
+            printf("ERROR; return code from pthread_join() is %d\n", rc);
+            exit(-1);
+        }
+        printf("Main: completed join with thread %ld having a status of %ld\n",i,(long)status);
+    }
+
     printf("Crunched\n");
+    sleep(1);
     _print_nodes(nodes,count);
 
     fclose(fp);
-    return EXIT_SUCCESS;
+    printf("Main is done.\n");
+    pthread_exit(NULL);
 }
