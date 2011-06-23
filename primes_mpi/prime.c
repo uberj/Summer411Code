bb4bd001586725dd762bed23c4a92d302e56677b
Kevin Strasser <strassek@onid.orst.edu>
2011-06-23 13:51:58 -0700

    now working on cluster

--- primes_mpi/prime.c
+++ primes_mpi/prime.c
@@ -1,72 +1,79 @@
-#include <pthread.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-int NUM_THREADS;
-int UBOUND; 
-
-int kPrimeCount;
-pthread_mutex_t mutexsum;
-
-void *findPrime(void *param)
-{
-	int i, j, test, tid, runningCount = 0;
-	tid = (int)param;
-
-     	for(i = (2 * tid) + 3; i < UBOUND; i += (2 * NUM_THREADS)){
-		test = (int)sqrt((double)i);
-		for(j = 2; j <= test; j++){
-			if(i % j == 0){
-				runningCount--;
-				break;
-			}
-		}
-		if(i != UBOUND){
-			runningCount++;
-		}
-	}
-	pthread_mutex_lock(&mutexsum);
-	kPrimeCount += runningCount;
-	pthread_mutex_unlock(&mutexsum);
-	pthread_exit((void*) tid);
-}
+#define TAG_INIT 31337
 
 int main(int argc, char *argv[])
 {
-	pthread_t thread[NUM_THREADS];
-	pthread_attr_t attr;
-	int rc, t;
-	void *status;
+	int i, j, ubound = 0, test, first, increment, kPrimeCount = 0, tPrimeCount = 0;
+	int mpiRank = 0, mpiSize = 1;
+	int namelen;
+	double t;
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
 
-	NUM_THREADS = atoi(argv[1]);
-        UBOUND = atoi(argv[2]);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+        MPI_Get_processor_name(processor_name, &namelen);
+	
+	fprintf(stderr, "Process %d started on host %s.\n", mpiRank, processor_name);
 
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-	pthread_mutex_init(&mutexsum, NULL);
-
-	kPrimeCount = 1; // skipping the first prime
-
-	for(t = 0; t < NUM_THREADS; t++){
-		printf("Main: creating thread %d\n", t);
-		rc = pthread_create(&thread[t], &attr, findPrime, (void *)t);
-		//TODO check for errors
+	if (!mpiRank){
+		if (argc > 1){
+        		ubound = atoi(argv[1]);
+		}
+                if (!ubound) {
+			printf("Upper bound not supplied, terminating.\n");
+		}
 	}
 
-	/* Free attribute and wait for the worker threads */
-	pthread_attr_destroy(&attr);
-	for(t = 0; t < NUM_THREADS; t++){
-		rc = pthread_join(thread[t], &status);
-		//TODO check for errors
+	MPI_Bcast(&ubound, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	if (!ubound){
+		MPI_Finalize();
+		return 255;
+	}
 
-		printf("Main: completed join with thread %d having a status of %ld\n", t, (long)status);
+	first = (mpiRank * 2) + 3; // this will be the first number this particular process tests
+        increment = mpiSize * 2; //  there is no need to test even numbers
+
+        t = MPI_Wtime();
+
+	for (i = first; i < ubound; i += increment){
+		test = (int)sqrt((double)i);
+		for(j = 2; j <= test; j++){
+			if(i % j == 0){
+				kPrimeCount--;
+				break;
+			}
+		}
+		if(i != ubound){
+			kPrimeCount++;
+		}
+	}
+
+	t = MPI_Wtime() - t;
+	printf("[Host: %s] process %d completed computation in %f.\n", processor_name, mpiRank, t);
+
+	if (!mpiRank){
+		kPrimeCount++; //becaue we skip 2
+		for(i=1;i<mpiSize;i++){
+        		MPI_Recv(&tPrimeCount, 1, MPI_INT, i, TAG_INIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			//printf("Receive: %d\n",tPrimeCount);
+			kPrimeCount += tPrimeCount;
+		}
+	}
+	else{
+		MPI_Send(&kPrimeCount, 1, MPI_INT, 0, TAG_INIT, MPI_COMM_WORLD);
+		//printf("Send: %d\n", kPrimeCount);
 	}
 	
-	printf("Main: program completed.\nFound %d primes in the range 0 - %d.\n Exiting.\n", kPrimeCount, UBOUND);
-	pthread_mutex_destroy(&mutexsum);
-	pthread_exit(NULL);
+	if (!mpiRank){
+		printf("All computation has finished.\n Found %d primes in the range 0 - %d.\nExiting.\n", kPrimeCount, ubound);
+	}
+
+	MPI_Finalize();
 }
 
 				
