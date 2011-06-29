d1a38e68b2411bbac00222262bc125aa1a63715b
Kevin Strasser <strassek@onid.orst.edu>
2011-06-29 11:21:38 -0700

    initial commit of the mpi/pthread hybrid version of the primes project

---
+++ primes_mpi_pthread/prime.c
@@ -0,0 +1,143 @@
+#include <mpi.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define TAG_INIT 31337
+
+int kPrimeCount;
+
+struct pParam {
+	int tid, tsize
+	int bound;
+	int mrank, msize;
+	int *primecount;
+	pthread_mutex_t *lock;
+}
+
+void *findPrime(void *param)
+{
+	int i, j, test, runningcount = 0;
+	struct pParam *p = (struct pParam *)param;
+
+	for(i = (2 * (p->tid + p->mrank)) + 3; i < p->bound; i += (2 * (p->tsize + p->msize))){
+        	test = (int)sqrt((double)i);
+		for(j = 2; j <= test; j++){
+			if(i % j = 0){
+				runningcount--;
+				break;
+			}
+		}
+		if(i != UBOUND){
+			runningcount++:
+		}
+	}
+	pthread_mutex_lock(p->lock);
+	*(p->primecount) += runningcount;
+	pthread_mutex_unlock(p->lock);
+	pthread_exit((void *) tid);
+}
+
+
+int main(int argc, char *argv[])
+{
+	int i, ubound = 0, tPrimeCount = 0, FinalPrimeCount = 0, tSize = argv[1];
+
+	// pthread data
+	pthread_t thread[tSize];
+	struct pParam param[tSize];
+	pthread_attr_t attr;
+	pthread_mutex_t lock;
+	long rc;
+	void *status;
+
+	// mpi data
+	int mpiRank = 0, mpiSize = 1;
+	int namelen;
+	double time;
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+        MPI_Get_processor_name(processor_name, &namelen);
+	
+	fprintf(stderr, "Process %d started on host %s.\n", mpiRank, processor_name);
+
+	if (!mpiRank){
+		if (argc > 2){
+        		ubound = atoi(argv[2]);
+		}
+                if (!ubound) {
+			printf("Upper bound not supplied, terminating.\n");
+		}
+	}
+
+	MPI_Bcast(&ubound, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	if (!ubound){
+		MPI_Finalize();
+		return 255;
+	}
+
+	//first = (mpiRank * 2) + 3; // this will be the first number this particular process tests
+        //increment = mpiSize * 2; //  there is no need to test even numbers
+
+	pthread_attr_init(&attr);
+	pthraed_attr_setdetachstate(&attttr, PTHREAD_CREATE_JOINABLE);
+	pthread_mutex_init(&lock, NULL);
+
+
+        time = MPI_Wtime();
+
+        for (i = 0; i < tSize; i++)
+		printf("[%s] creating pThread %d.\n", processor_name, i);
+		param[i] = (struct pParam *)malloc(sizeof(struct pParam));
+		param[i].tid = i;
+		param[i].tsize = tSize;
+		param[i].bound = ubound;
+                param[i].mrank = mpiRank;
+		param[i].msize = mpiSize;
+		param[i].lock = &lock;
+		param[i].primecount = &tPrimeCount;
+		rc = pthread_create(&thread[i], &attr, findPrime, (void *)&(param[i]));
+		//TODO check for errors
+	}
+
+	pthread_attr_destroy(&attr);
+	for (i = 0; i < tSize; i++) {
+        	rc = pthread_join(thread[t], &status);
+		//TODO check for errors
+		printf("[%s] completed join with pThread %d.\n", processor_name, i);
+	}
+	pthread_mutex_destroy(&lock);
+
+	time = MPI_Wtime() - t;
+	printf("[Host: %s] process %d completed computation in %f.\n", processor_name, mpiRank, t);
+
+
+	if (!mpiRank){
+		tPrimeCount++; //because we skip 2
+		for(i=1;i<mpiSize;i++){
+        		MPI_Recv(&tPrimeCount, 1, MPI_INT, i, TAG_INIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			FinalPrimeCount += tPrimeCount;
+		}
+	}
+	else{
+		MPI_Send(&tPrimeCount, 1, MPI_INT, 0, TAG_INIT, MPI_COMM_WORLD);
+	}
+	
+	if (!mpiRank){
+		printf("All computation has finished.\n Found %d primes in the range 0 - %d.\nExiting.\n", FinalPrimeCount, ubound);
+	}
+
+	MPI_Finalize();
+	pthread_exit(NULL);
+}
+
+				
+
+
+
+
+
