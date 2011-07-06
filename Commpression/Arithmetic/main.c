09dfc0f687bd6af1ca292af8fa49a04f19b2ff2b
uberj <uberj@travisty.411lab.com>
2011-07-06 11:17:58 -0700

    writing correct binary data to file. decoding needs to be written.

--- Commpression/Arithmetic/main.c
+++ Commpression/Arithmetic/main.c
@@ -5,24 +5,35 @@
 #include "model.c"
 
 int main( int argc, char **argv ) {
-    FILE *file;
+    FILE *out_file;
+    FILE *in_file;
     char *fname;
     if(argc > 1){
         fname = argv[1];
         fprintf(stdout, "Filename is %s\n", fname);
     }
-    file = fopen(fname, "r");
-    if(!file){
+    in_file = fopen(fname, "r");
+    out_file = fopen("compressed", "w+");
+    if(!out_file || !in_file ){
         perror("Could not open file");
         return 1;
     }
-    fprintf(stdout, "File %s was open success fully\n", fname);
-    
+    fprintf(stdout, "File (%s) were opened successfully\n", fname);
+
     struct model mdl;
-    populate_model( file, &mdl );
+    mdl.out_fp = out_file;
+    mdl.in_fp = in_file;
+
+
+    populate_model( in_file, &mdl );
     init_shifting_model( &mdl );
-    fseek( file, 0 , SEEK_SET ); //Reset file
-    encode_with_model( file, &mdl );
+    fseek( in_file, 0 , SEEK_SET ); //Reset file
+    encode_with_model( in_file, &mdl );
+
+
+    fseek( out_file, 0 , SEEK_SET ); //Reset file
+    init_decode( &mdl );
+    decode_with_model( &mdl );
     //print_model(&mdl);
     return 0;
 
--- Commpression/Arithmetic/model.c
+++ Commpression/Arithmetic/model.c
@@ -8,9 +8,19 @@
 #include "model.h"
 #define SB 7
 #define MODEL_SIZE 254
+#define BUF_SIZE 32
 // Better to over shoot. I'm using unsigned longs.
 
+static unsigned char bit_buff;
+static unsigned char bit_pos;
+static unsigned char en_code[BUF_SIZE];
+static unsigned char de_code[BUF_SIZE];
+static unsigned int en_code_pos;
+static unsigned int de_code_pos;
+
 struct model {
+    FILE *out_fp;
+    FILE *in_fp;
     unsigned long* symbols;
     unsigned long total;
     unsigned long mHigh;
@@ -35,7 +45,67 @@ void print_model( struct model* mdl );
 /* Using a model and a file, encode to a stream of bits.
  */
 void encode_with_model( FILE *fp, struct model* mdl );
-void setBit( struct model *mdl, int bit );
+void set_bit( struct model *mdl, int bit );
+void flush_bit_buff( struct model *mdl);
+void decode_with_model( struct model* mdl );
+void fill_bit_buff( struct model *mdl );
+
+int get_bit( struct model *mdl ) {
+    unsigned char clean = 0; //Use this guy to get bit values
+    if ( de_code_pos >= 32)
+        fill_bit_buff( mdl );
+
+    switch ( bit_pos ) {
+        case 0:
+            clean = de_code[de_code_pos] & '\1';
+            break;
+        case 1:
+            clean = de_code[de_code_pos] & '\2';
+            break;
+        case 2:
+            clean = de_code[de_code_pos] & '\4';
+            break;
+        case 3:
+            clean = de_code[de_code_pos] & 0x8;
+            de_code_pos++;
+            bit_pos = 0;
+            return clean; //Get out!
+    }
+    bit_pos++;
+    return clean;
+
+}
+void fill_bit_buff( struct model *mdl ){
+    int i;
+    char sym;
+    for( i=0, sym = fgetc(mdl->out_fp); i < BUF_SIZE && sym != EOF ; i++, sym = fgetc(mdl->out_fp) ) {
+        de_code[i] = sym;
+    }
+    de_code_pos = 0;
+    bit_pos = 0;
+
+}
+
+
+void decode_with_model( struct model* mdl ) {
+    int i = 0;
+    //unsigned long low_count;
+    for( i=0; i<20; i++){
+        if( get_bit( mdl ) )
+            printf("1");
+        else
+            printf("0");
+    }
+    //encode_finish( mdl );
+    printf("\n");
+}
+
+void init_decode( struct model *mdl ) {
+    bit_buff = 0;
+    bit_pos = 0;
+    de_code_pos = 0;
+    fill_bit_buff( mdl );
+}
 
 void encode_finish( struct model *mdl ) {
     unsigned long g_FirstQuarter = mdl->g_FirstQuarter;
@@ -49,13 +119,19 @@ void encode_finish( struct model *mdl ) {
      * Case 2: low < Half < thirdQuarter <= high
      */
     if( mLow < g_FirstQuarter ) {
-        setBit( mdl , 0 );
+        set_bit( mdl , 0 );
         for( i=0; i < mScale; i++) {
-            setBit( mdl, 1 );
+            set_bit( mdl, 1 );
         }
     } else {
-        setBit( mdl , 1 ); //Decoder add's zero's
+        set_bit( mdl , 1 ); //Decoder add's zero's
     }
+    flush_bit_buff( mdl );
+
+    //Debugging - print stuff (This will eventually need to be written to a file for
+    //the decoder to use)
+    printf("mHigh: %lu\n", mdl->mHigh );
+    printf("mLow: %lu\n", mdl->mLow );
 
 }
 
@@ -74,22 +150,22 @@ void do_one_encode( struct model *mdl, unsigned long low_count, unsigned long hi
     //E1-E2 Scale
     while( ( mHigh < g_Half ) || ( mLow >= g_Half ) ) {
         if( mHigh < g_Half ) { //E1
-            setBit( mdl , 0 );
+            set_bit( mdl , 0 );
             mLow = mLow * 2;
             mHigh = mHigh * 2 + 1;
 
             // Check for E3
             for(;mScale > 0; mScale--){
-                setBit( mdl , 1 );
+                set_bit( mdl , 1 );
             }
         } else if( mLow >= g_Half ) { //E2
-            setBit( mdl, 1 );
+            set_bit( mdl, 1 );
             mLow = 2 * ( mLow - g_Half );
             mHigh = 2 * ( mHigh - g_Half ) + 1;
 
             // Check for E3
             for(;mScale > 0; mScale--){
-                setBit( mdl , 0 );
+                set_bit( mdl , 0 );
             }
         }
     }
@@ -134,8 +210,68 @@ void encode_with_model( FILE *fp, struct model* mdl ){
     encode_finish( mdl );
 }
 
-void setBit( struct model *mdl, int bit ) {
-    printf("%i",bit);
+void flush_bit_buff( struct model *mdl ) {
+    int i;
+    en_code[en_code_pos] = bit_buff;
+    // Last write
+    fwrite( &bit_buff, 1, 1, mdl->out_fp );
+    en_code_pos++;
+    //Debugging stuff
+    printf("last byte: %d\n",bit_buff);
+    printf("And buffer is...\n");
+    for( i=0; i<en_code_pos ; i++ ){
+        printf("%d ",en_code[i]);
+    }
+    printf("\n");
+}
+
+/*
+ * Notes: Right now set_bit writes to a file when it has an entire byte. Idealy it
+ * should write to the file after en_code becomes full. This will save time by avoiding
+ * uneeded i/o requests.
+ */
+void set_bit( struct model *mdl, int bit ) {
+    switch ( bit_pos ) {
+        case 0:
+            if ( bit ){
+                bit_buff |= '\1';printf("0");
+            } else {
+                printf("X");
+            }
+            break;
+        case 1:
+            if ( bit ){
+                bit_buff |= '\2';printf("1");
+            }else {
+                printf("X");
+            }
+
+            break;
+        case 2:
+            if ( bit ){
+                bit_buff |= '\4';printf("2");
+            }else {
+                printf("X");
+            }
+
+            break;
+        case 3:
+            if ( bit ){
+                bit_buff |= 0x8;printf("3");
+            }else {
+                printf("X");
+            }
+
+            // We need a new byte. Eventually we should store more than one byte. For now just write to file.
+            printf("new byte: %d\n",bit_buff);
+            fwrite( &bit_buff, 1, 1, mdl->out_fp );
+            en_code[en_code_pos] = bit_buff;
+            en_code_pos++;
+            bit_buff = 0x0;
+            bit_pos = 0;
+            return; //Get out!
+    }
+    bit_pos++;
 }
 
 void init_shifting_model( struct model *mdl ) {
@@ -145,6 +281,9 @@ void init_shifting_model( struct model *mdl ) {
     mdl->mLow = 0;
     mdl->mHigh = 0x7F;
     mdl->mScale = 0;
+    bit_pos = 0;
+    bit_buff = 0;
+    en_code_pos = 0;
 }
 
 void populate_model( FILE *fp, struct model* mdl ) {
