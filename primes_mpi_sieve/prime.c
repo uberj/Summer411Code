2a25508ff0c5e72d66e0baf5b5e730a4cec14e24
Kevin Strasser <strassek@onid.orst.edu>
2011-07-14 13:39:01 -0700

    bug fixes

--- primes_mpi_sieve/prime.c
+++ primes_mpi_sieve/prime.c
@@ -13,12 +13,16 @@
 #define ARRAY_TYPE int
 #define INDEX_TYPE int
 
+#define MASTER mpiRank == 0
+#define SLAVE mpiRank != 0
+#define TAG_INIT 31336
+
 int main(int argc, char *argv[])
 {
         INDEX_TYPE i = 0, n, c1, c2, c3;
-	int namelen, mpiRank = 0, mpiSize = 1;
+	int temp, namelen, mpiRank = 0, mpiSize = 1;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
-
+        const int finished = 0;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
@@ -26,7 +30,7 @@ int main(int argc, char *argv[])
 
 	printf("[Host %s] mpiRank = %d\n", processor_name, mpiRank);
         
-	if(!mpiRank && argc < 2){
+	if(MASTER && argc < 2){
 		printf("[Host %s] usage: %s bound\nExiting.", processor_name, argv[0]);
 	} else {
 		i = atoi(argv[1]);
@@ -57,22 +61,33 @@ int main(int argc, char *argv[])
         prime[1]=COMPOSITE;
         prime[2]=PRIME;
  
-        //find primes then eliminate their multiples (0 = prime, 1 = composite)
-        for(c2 = (mpiRank * 2) + 3;c2 <= (int)sqrt(i)+1;c2 += mpiSize * 2){
-                if(prime[c2] == PRIME){
-                        c1=c2;
-                        for(c3 = 2*c1; c3 <= i; c3 = c3+c1){
-                                prime[c3] = COMPOSITE;
-				MPI_Reduce(prime, prime_final, c3, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-                        }
-                }
-        }
+ 	if(SLAVE){
+        	//find primes then eliminate their multiples (0 = prime, 1 = composite)
+        	for(c2 = (mpiRank * 2) + 3;c2 <= (int)sqrt(i)+1;c2 += mpiSize * 2){
+                	if(prime[c2] == PRIME){
+                        	c1=c2;
+                        	for(c3 = 2*c1; c3 <= i; c3 = c3+c1){
+                                	prime[c3] = COMPOSITE;
+					MPI_Send(&c3, 1, MPI_INT, 0, TAG_INIT, MPI_COMM_WORLD);
+                        	}
+                	}
+        	}
+		MPI_Send(&finished, 1, MPI_INT, 0, TAG_INIT, MPI_COMM_WORLD);
+	} else { //this is the master
+        	while(mpiSize > 1){
+			MPI_Recv(&temp, 1, MPI_INT, MPI_ANY_SOURCE, TAG_INIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			if(temp)
+				prime[temp] = COMPOSITE;
+			else
+				mpiSize--;
+		}
+	}
 
 	//allow all processes to finish
         MPI_Barrier(MPI_COMM_WORLD);
 
         //print primes
-	if(mpiRank == 0){
+	if(MASTER){
 		n = 0;
         	for(c1 = 0; c1 < i+1; c1++)
                 	if(prime_final[c1] == PRIME) n++;
