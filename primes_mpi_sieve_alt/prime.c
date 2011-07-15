f96e287ece4fe7246eaa9281ea7171ffb28455a9
Kevin Strasser <strassek@onid.orst.edu>
2011-07-15 15:05:53 -0700

    bug fixes

--- primes_mpi_sieve_alt/prime.c
+++ primes_mpi_sieve_alt/prime.c
@@ -17,8 +17,8 @@
 #define SLAVE(id) (id != 0)
 
 #define BLOCK_LOW(id,p,n) ((id) * (n) / (p))
-#define BLOCK_HIGH(id,p,n) (((id + 1) * n / p) - 1)
-#define BLOCK_SIZE(id,p,n) (BLOCK_HIGH(id,p,n) - BLOCK_LOW(id,p,n))
+#define BLOCK_HIGH(id,p,n) (BLOCK_LOW((id)+1,p,n) - 1)
+#define BLOCK_SIZE(id,p,n) (BLOCK_LOW(id + 1,p,n) - BLOCK_LOW(id,p,n))
 #define BLOCK_OWNER(index,p,n) (((p)*((index)+1)-1)/(n))
 
 int main(int argc, char *argv[])
